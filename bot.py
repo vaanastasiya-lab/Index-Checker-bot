5ac4ec5eebbdc452c946e3fd1299943b383e0825
vaanastasiya-lab <v.a.anastasiya@gmail.com>
2026-07-15 17:03:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,8 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 # --- НАСТРОЙКИ ---
-BOT_TOKEN = "8757758492:AAEOGGor6d9ON7gyqrk_K769OpwhcbUdteE"
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 USER_IDS = [5295327437, 6964867018]
 
 THRESHOLDS = {"moex": 2.0, "vtb": 3.0, "brent": 2.0, "spacex": 3.0}
