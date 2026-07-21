55cccd95d9f62c3312273c65bd05cbfdd45ee53b
vaanastasiya-lab <v.a.anastasiya@gmail.com>
2026-07-21 15:36:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import os
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 # Импортируем из правильного места в aiogram 3.x
-from aiogram.utils.html import bold
+from aiogram.html import bold
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 # --- НАСТРОЙКИ ---
