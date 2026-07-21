23c4ad3ff59581612b26181cc8003315764125ed
vaanastasiya-lab <v.a.anastasiya@gmail.com>
2026-07-21 16:07:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 # --- НАСТРОЙКИ ---
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Список ID пользователей для автоматических уведомлений (ЗАПОЛНИТЕ ТУТ)
-USER_IDS = 
+USER_IDS = [5295327437, 6964867018]
 
 # Процентные пороги для автоматических алармов
 THRESHOLDS = {"moex": 2.0, "vtb": 3.0, "brent": 2.0, "spacex": 3.0}
